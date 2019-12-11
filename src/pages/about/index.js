1f3d7a389db6ec45de0c632b20bd88e29bbb04a6
MAGLeb <maximovglebigorevich@gmail.com>
2019-12-11 17:38:52 +0300

    edit cards style and html, refactoring

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,9 +2,10 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable no-unused-vars */
 import './style.css'
-import '../../components/menu'
 import Flickity from 'flickity'
 
+
+// Slider settings
 const carousel = document.querySelector('.slider')
 
 const hanlerDynamicGroupCell = () => (window.screen.width < 1280
@@ -17,20 +18,3 @@ const flkty = new Flickity(carousel, {
   selectedAttraction: 0.01,
   friction: 0.15,
 })
-
-// show/hide history__link and history_button
-const historyButton = document.querySelector('.history__button')
-const historyTitleLink = document.querySelector('.history__title-link')
-
-function handleShowHideHistoryButton(event) {
-  if (window.screen.width < 768) {
-    historyButton.classList.remove('history__button-hide')
-    historyTitleLink.classList.add('history__title-link-hide')
-  } else {
-    historyButton.classList.add('history__button-hide')
-    historyTitleLink.classList.remove('history__title-link-hide')
-  }
-}
-
-window.onresize = () => handleShowHideHistoryButton()
-window.addEventListener('resize', handleShowHideHistoryButton())
