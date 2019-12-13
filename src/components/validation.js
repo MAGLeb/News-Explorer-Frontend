acfde00bc5e121b02d1106934ea8a0555cf80fca
MAGLeb <maximovglebigorevich@gmail.com>
2019-12-13 16:53:39 +0300

    add validation for froms

---
+++ src/components/validation.js
@@ -0,0 +1,43 @@
+export default function checkInputs(form) {
+  const inputs = Array.from(form.querySelectorAll('input'))
+  const errors = Array.from(form.querySelectorAll('.popup__error'))
+  const button = form.querySelector('button')
+
+  const validateEmail = (email) => {
+    const regular = /^(([^<>()[\]\\.,:\s@\"]+(\.[^<>()[\]\\.,:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regular.test(email)
+  }
+
+  inputs.forEach((item, index) => {
+    item.addEventListener('input', () => {
+      if (item.name === 'email') {
+        if (!validateEmail(item.value)) {
+          errors[index].textContent = 'Неправильный формат email'
+          errors[index].classList.add('popup__error_active')
+        }
+        errors[index].classList.remove('popup__error_active')
+      }
+
+      if (item.value === '') {
+        errors[index].textContent = 'Это обязательное поле'
+        errors[index].classList.add('popup__error_active')
+      } else if (item.value.length === 1) {
+        errors[index].classList.add('popup__error_active')
+        errors[index].textContent = 'Должно быть от 2 до 30 символов'
+      } else if (item.value.length <= 2) {
+        errors[index].classList.remove('popup__error_active')
+      }
+
+      const checkInputsValue = inputs.every((input) => input.value !== '')
+      const checkErrorClass = errors.every((error) => !error.classList.contains('popup__error_active'))
+
+      if (checkInputsValue && checkErrorClass) {
+        button.classList.add('popup__button_active')
+        button.removeAttribute('disabled')
+      } else {
+        button.classList.remove('popup__button_active')
+        button.setAttribute('disabled')
+      }
+    })
+  })
+}
